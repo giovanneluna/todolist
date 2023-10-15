088d4efebfec0d0dff538df34a6fc7acc27431b7
Giovanne de Luna <gikalunalticg@gmail.com>
2023-10-15 15:29:12 -0300

    First Commit

---
+++ src/test/java/br/com/giovanneluna/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.giovanneluna.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
